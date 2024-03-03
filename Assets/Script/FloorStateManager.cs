c80bc56cf4c94b4adf921cc9448a2c27981e6387
DCoder <david347lj@gmail.com>
2024-03-03 16:10:10 +0100

    Añadido ScoreManager

--- Assets/Script/FloorStateManager.cs
+++ Assets/Script/FloorStateManager.cs
@@ -1,31 +1,21 @@
-using System.Buffers.Text;
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class FloorStateManager : MonoBehaviour
 {
     [SerializeField] GameObject floor;
     [SerializeField] GameObject price;
     [SerializeField] GameObject gem;
-    [SerializeField] PlayerController player;
-    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] ScoreManager scoreManager;
     private float valX = 0f;
     private float valZ = 0f;
     private float valY = 0f;
-    private int phase = 0;
     private float starY= 0f;
   
     private float valueToIncrease;
-    private int totalpoints=0;
-    private float baseSpeed = 0;
     private const string FLOOR_TAG = "Floor";
     private const string PRICE_TAG = "price";
     private const string GEM_TAG = "Gem";
-    private float speedMultiplier = 1;
     void Start()
     {
         valueToIncrease = floor.transform.localScale.z;
@@ -33,36 +23,13 @@ public class FloorStateManager : MonoBehaviour
         valZ = floor.transform.position.z;
         valY = floor.transform.position.y;
         starY = 0.5f;
-        baseSpeed = player.speed;
         CreateInitialFloor();
     }
 
     // Update is called once per frame
     void Update()
     {   
-        scoreText.text = "Score: " + totalpoints;
-        player.speed = baseSpeed * speedMultiplier;
-
-        if (totalpoints > 140 && phase == 3){
-            speedMultiplier = 1.8f;
-            phase++;
-            return;
-        }
-        if (totalpoints > 70 && phase == 2){
-            speedMultiplier = 1.5f;
-            phase++;
-            return;
-        }
-        if (totalpoints > 50 && phase == 1){
-            speedMultiplier = 1.25f;
-            phase++;
-            return;
-        }
-        if (totalpoints > 20 && phase == 0){
-            speedMultiplier = 1.1f;
-            phase++;
-            return;
-        }
+        
     }
 
     void CreateInitialFloor()
@@ -78,7 +45,7 @@ public class FloorStateManager : MonoBehaviour
     {
         if (other.gameObject.CompareTag(FLOOR_TAG))
         {
-            totalpoints++;
+            scoreManager.Score++;
             StartCoroutine(DeleteFloor(other.gameObject));
         }
 
@@ -88,16 +55,15 @@ public class FloorStateManager : MonoBehaviour
     {
         if (other.gameObject.CompareTag(PRICE_TAG))
         {
-            
-            totalpoints+=3;
+            scoreManager.Score+=3;
             Destroy(other.gameObject);
         }
         else
         {
             if (other.gameObject.CompareTag(GEM_TAG))
             {
-                totalpoints++;
-                Debug.Log(totalpoints);
+                scoreManager.Score++;
+                Debug.Log(scoreManager.Score);
                 Destroy(other.gameObject);
             }
         }
