2eb7b7081c31bb5adc45b7cde8fa019a269eb99c
alemale2001 <79916469+alemale2001@users.noreply.github.com>
2024-03-04 19:40:16 +0100

    Visuales
    Fondo, textura suelo y fuente del score, aún trabajando con el canvas de la primera escena

--- Assets/Script/CuentaAtras.cs
+++ Assets/Script/CuentaAtras.cs
@@ -15,6 +15,7 @@ public class CuentaAtras : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _image.gameObject.SetActive(false);
         _button.onClick.AddListener(StartCountdown);               
     }
 
